b5a28715e70087548de5ce4a75544d22e6c0e673
Ocysellus <53922987+Ocysellus@users.noreply.github.com>
2021-04-03 20:18:32 -0700

    Preliminary build.zig commenting
    Since I'm new to Zig, I added some comments that outline my current understanding of the `build.zig` file and what each statement accomplishes.
    I hope to improve upon this initial commit as I read through the Zig documentation and update this file with more accurate and explanatory information.

--- build.zig
+++ build.zig
@@ -1,15 +1,26 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
+    // Get a build mode from the builder
+    // build mode?
     const mode = b.standardReleaseOptions();
+    // Create an executable using the builder
+    // arg 0 = file / project name?
+    // arg 1 = main file location?
     const exe = b.addExecutable("ziget", "src/main.zig");
+    // sets the build mode for the executable?
     exe.setBuildMode(mode);
 
+    // Create the run command for the executable?
     const run_cmd = exe.run();
 
+    // Set up the run steps?
     const run_step = b.step("run", "Run the app");
+    // In order to run the run step you need to have the run_cmd?
     run_step.dependOn(&run_cmd.step);
 
+    // Builder's default step must depend on the execution steps?
     b.default_step.dependOn(&exe.step);
+    // Install the artifact?
     b.installArtifact(exe);
 }
