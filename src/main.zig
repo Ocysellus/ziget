79ffbcae91fe5d41e157541a1247e3c8b50b3771
Ocysellus <53922987+Ocysellus@users.noreply.github.com>
2021-04-03 20:15:28 -0700

    Providing comments for personal understanding
    Since I'm still learning how to use Zig, I added some comments and changed the formatting of the print outs.
    
    I hope to update these comments with more accurate and descriptive information as I refer to the Zig language documentation.

--- src/main.zig
+++ src/main.zig
@@ -10,38 +10,64 @@ const ZigetError = error{
 };
 
 pub fn main() anyerror!void {
+    // Creating an arean using the page allocator?
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // Skip the step where the arena page is deinitialized?
     defer arena.deinit();
 
+    // Save the arena page's allocator?
     var allocator = &arena.allocator;
+    // Get an iterator for the processes' arguments?
     var args_it = std.process.args();
 
-    // skip args[0]
-
+    // skip args[0] this is the process name.
     _ = args_it.skip();
 
+    // Store the name of the host and allocate it using our page allocator?
     const host = try (args_it.next(allocator) orelse {
+        // if we fail to store the host name, then that argument is missing
+        // stop execution since we need an argument for the host.
         std.debug.warn("no host provided\n", .{});
         return error.InvalidArgs;
     });
 
+    // For personal understanding print the type of "host"
+    std.debug.warn("Type of host: {s}\n", .{@TypeOf(host)});
+
+    // Store the name of the remote path and allocate it using our page allocator?
     const remote_path = try (args_it.next(allocator) orelse {
         std.debug.warn("no remote path provided\n", .{});
         return error.InvalidArgs;
     });
 
+    // For personal understanding print the type of "remote_path"
+    std.debug.warn("Type of remote_path: {s}\n", .{@TypeOf(remote_path)});
+    
+    // Store the output path and allocate it using our page allocator?
     const output_path = try (args_it.next(allocator) orelse {
         std.debug.warn("no path provided\n", .{});
         return error.InvalidArgs;
     });
 
-    std.debug.warn("host: {s} remote: {s} output path: {s}\n", .{ host, remote_path, output_path });
+    // For personal understanding print the type of "output_path"
+    std.debug.warn("Type of output_path: {s}\n", .{@TypeOf(output_path)});
+    
+    // Print out the arguments we received.
+    std.debug.warn("host: {s}\nremote: {s}\noutput path: {s}\n", .{ host, remote_path, output_path});
 
+    // Attempt to connect to the host.
+    // arg 0: Provided allocator
+    // arg 1: The host to connect to
+    // arg 2: The port to connect to
     var conn = try std.net.tcpConnectToHost(allocator, host, 80);
+    // Skip closing the connection?
     defer conn.close();
 
+    // Create a buffer of 256 characters
     var buffer: [256]u8 = undefined;
+    // Get the base http?
     const base_http = "GET {s} HTTP/1.1\r\nHost: {s}\r\nConnection: close\r\n\r\n";
+    // Store the message?
     var msg = try std.fmt.bufPrint(&buffer, base_http, .{ remote_path, host });
 
     _ = try conn.write(msg);
